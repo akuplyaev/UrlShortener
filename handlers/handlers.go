3f2e84e45f1edfdf2e3706803272c11fa8e73b14
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2017-07-11 04:34:10 +0300

    Redirect

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,23 +7,23 @@ import (
 )
 //http://localhost:8080/a?url=http%3A%2F%2Fgoogle.com%2F%3Fq%3Dgolang
 //храним значения key=shortUrl=value=longUrl
-func AddShortUrl (response http.ResponseWriter,request *http.Request){
+func AddShortUrl (resp http.ResponseWriter,request *http.Request){
 	params:=request.URL.Query()
 	longUrl:=params.Get("url")
 	if longUrl == "" {  //придумать как проверить этот url на корекктность
-		http.Error(response, "Bad Request", http.StatusBadRequest)
+		http.Error(resp, "Bad Request", http.StatusBadRequest)
 		return
 	}
 	shortUrl:=storage.FindShortUrl(longUrl)
-	response.WriteHeader(http.StatusOK)
-	fmt.Fprintf(response,"%s",shortUrl)
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprintf(resp,"%s",shortUrl)
 }
-func RedirectUrl(responce http.ResponseWriter,request *http.Request) {
+func RedirectUrl(resp http.ResponseWriter,request *http.Request) {
 	vars := mux.Vars(request)
 	shortUrl := vars["shortUrl"]
     longUrl:=storage.FindLongUrl(shortUrl)
 	if longUrl==""{
-          http.Error(responce, "Bad Request ",http.StatusBadRequest)
+          http.Error(resp, "Bad Request ",http.StatusBadRequest)
 	}
-	http.RedirectHandler(longUrl,302)
+	http.Redirect(resp,request,longUrl,http.StatusFound)
 }
